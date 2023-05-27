0bcae055dad9df8a037ce1a90ba4ae914a60e6ed
AdiJigit <adiletgylzhigit@gmail.com>
2023-05-27 03:22:08 -0800

    first commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -19,40 +19,40 @@ const Skills = ({}: Props) => {
   return (
     <div id='skills' className="max-h-[100vh]">
       <Carousel dynamicHeight={true}>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={ReactImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={JSImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={CSSImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={HTMLImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={SassImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={MongoImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={SQLImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={RestAPIImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={GraphQLImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={GithubImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={NextJSImg} />
         </div>
-        <div className="w-max m-auto">
+        <div className="md:w-max m-auto">
           <img src={TailwindCSSImg} />
         </div>
       </Carousel>
